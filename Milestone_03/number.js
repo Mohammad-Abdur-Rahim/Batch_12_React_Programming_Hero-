6fd78b6c98ae88c2c440dfc6fe45c42ff94f400e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-16 22:29:22 +0600

    * Number Fundamental concept *

---
+++ Milestone_03/number.js
@@ -0,0 +1,13 @@
+let orangePrice = 100 ;
+// let choclate = parseInt("240") ;
+let choclate = parseFloat("240.55") ;
+
+// console.log(typeof orangePrice);
+// console.log(typeof choclate);
+
+console.log(orangePrice+choclate);
+
+let first =0.1;
+let second =0.2;
+let total = first + second ;
+console.log(total.toFixed(2));
