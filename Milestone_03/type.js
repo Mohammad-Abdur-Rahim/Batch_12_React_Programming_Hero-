b8d58dc501bb165b92e05fa9f6e5a72d02902968
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-16 21:59:51 +0600

    Data type concept

---
+++ Milestone_03/type.js
@@ -0,0 +1,11 @@
+// String Type .
+let myname = 'Md Abdur Rahim';
+console.log(myname);
+
+// Boolean Type..
+let isEngineer = true ;
+console.log(isEngineer);
+
+// How to check Data Type .....
+console.log(typeof myname);
+console.log(typeof isEngineer);
