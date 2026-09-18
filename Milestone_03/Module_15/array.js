524c0ece065ff8ec3caa7b6eeeba9f0f28496251
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-18 23:02:56 +0600

    * Push/pop shift/unshift *

--- Milestone_03/Module_15/array.js
+++ Milestone_03/Module_15/array.js
@@ -1,5 +1,15 @@
 //Array Basic ..........
 
 const numbers = [10,22,34,54,656,89];
+
+// numbers.push(15);
+// numbers.pop(15);
+numbers.unshift(15);
+numbers.shift();
+
 console.log(numbers);
-console.log(numbers.length);
+
+
+
+// console.log(numbers);
+// console.log(numbers.length);
