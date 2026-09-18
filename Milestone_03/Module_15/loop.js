6bf791bc52d7f8488219a81e0b0d716141af6461
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-18 11:20:32 +0600

    For loop Odd number

--- Milestone_03/Module_15/loop.js
+++ Milestone_03/Module_15/loop.js
@@ -2,8 +2,8 @@
 
 // Even numbers
 let sum = 0;
-for(let i = 0;i<=10;i=i+2){
+for(let i = 1;i<=10;i=i+2){
     console.log(i);
     sum=sum+i;
 }
-console.log('Even number sum  : ', sum);
+console.log('ODD number sum  : ', sum);
