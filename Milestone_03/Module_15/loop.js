08f24589b63adfb5004d29041fec7b4aeba7dafa
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-18 11:19:56 +0600

    For loop even number

--- Milestone_03/Module_15/loop.js
+++ Milestone_03/Module_15/loop.js
@@ -1,5 +1,9 @@
 // For Loop-------------------------
 
-for(let num =0;num<10;num++){
-    console.log(num);
+// Even numbers
+let sum = 0;
+for(let i = 0;i<=10;i=i+2){
+    console.log(i);
+    sum=sum+i;
 }
+console.log('Even number sum  : ', sum);
