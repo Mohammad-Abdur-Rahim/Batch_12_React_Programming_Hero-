c52002e87181976685ab7ef8f42656cf7d46b37c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-18 23:32:56 +0600

    String Basic

--- Milestone_03/Module_16/object.js
+++ Milestone_03/Module_16/object.js
@@ -1,1 +1,10 @@
-console.log('working');
+// String & Object ....................
+
+const country = 'Bangladesh';
+const division = "Dhaka";
+const backtik = `This is Backtik String`;
+
+const thana = new String ('Uttara');
+
+console.log(typeof(backtik));
+console.log(typeof(thana));
