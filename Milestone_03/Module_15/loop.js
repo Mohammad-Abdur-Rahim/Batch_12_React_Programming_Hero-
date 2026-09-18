b511db9a0e8f97fc078e83ab058f98895e8cf7bf
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-18 10:45:10 +0600

    sum of even number

--- Milestone_03/Module_15/loop.js
+++ Milestone_03/Module_15/loop.js
@@ -3,7 +3,10 @@ let num = 1 ;
 let sum = 0;
 while(num <= 10){
     console.log(num);
-    sum = sum + num ;
-    console.log('sum of number : ', sum);
+    if(num % 2 === 0){
+        sum= sum + num ;
+      
+    }
     num++;
+    console.log('sum of even number : ', sum);
 }
