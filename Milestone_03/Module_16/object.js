49ee5595811b2c6c5aeeb79bbfe72fdbfc5712e7
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-21 12:12:19 +0600

    Case sensetive

--- Milestone_03/Module_16/object.js
+++ Milestone_03/Module_16/object.js
@@ -1,10 +1,11 @@
 // String & Object ....................
 
-const country = 'Bangladesh';
-const division = "Dhaka";
-const backtik = `This is Backtik String`;
+const subject ='Programming';
+const book = 'programming'
 
-const thana = new String ('Uttara');
-
-console.log(typeof(backtik));
-console.log(typeof(thana));
+if (subject === book){
+    console.log('Same to same value');
+}
+else{
+    console.log('nothing');
+}
