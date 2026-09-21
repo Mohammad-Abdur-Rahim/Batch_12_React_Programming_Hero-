7ffb3c94c0b810f62cc2fc5d392ad89cdb550d5b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-21 20:30:52 +0600

    Dot/Bracket access Object

--- Milestone_03/Module_16/object.js
+++ Milestone_03/Module_16/object.js
@@ -1,10 +1,18 @@
 //  Object ....................
 
-const phone ={
-    name : 'samsung',
-    price: 34000,
-    color: 'blue'
+const person ={
+    name : 'Md Abdur Rahim',
+    age: 25,
+    profession : 'Website Developer',
+    salary : 45000,
+    married : false ,
+    date : '21 September 2026'
 };
 
+person.salary = 245000 ; 
+person['age'] = 27                      // object value update system dot/[]
 
-console.log(phone);
+// console.log(person);
+console.log(person.name);            // Dot Notation...
+console.log(person['age']);         // Bracket notation best practice...
+console.log(person['salary']);     // Bracket notation best practice...
