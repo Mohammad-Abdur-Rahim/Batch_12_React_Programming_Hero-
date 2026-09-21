a330fa922c4c08a780af06ffdff05ae876f95104
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2026-09-21 12:23:51 +0600

    Slice String

--- Milestone_03/Module_16/object.js
+++ Milestone_03/Module_16/object.js
@@ -3,9 +3,4 @@
 const subject ='Programming';
 const book = 'programming'
 
-if (subject.toLocaleLowerCase() === book.toLocaleLowerCase()){
-    console.log('Same to same value');
-}
-else{
-    console.log('nothing');
-}
+console.log(subject.slice(0,7));
